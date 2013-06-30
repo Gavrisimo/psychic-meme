73dc771461b9d5ef28998c644d111b0ffa54ada2
Milos Gavrilovic <milos@gavrilovic.rs>
2013-06-30 22:48:17 +0200

    Update and run grunt build
    Ref #1

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,13 @@ module.exports = function(grunt) {
       options: {
         separator: ';'
       },
+      app: {
+        src: [
+          'assets/js/app/main.js',
+          'assets/js/app/document-manager.js'
+        ],
+        dest: 'assets/js/<%= pkg.name %>-app.js'
+      },
       vendor: {
         src: [
           'assets/js/libs/jquery-2.0.2.min.js',
@@ -18,6 +25,11 @@ module.exports = function(grunt) {
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> libs - <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      app: {
+        files: {
+          'assets/js/<%= pkg.name %>-app.min.js': ['<%= concat.app.dest %>']
+        }
       },
       vendor: {
         files: {
